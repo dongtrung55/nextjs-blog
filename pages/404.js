5dea8be1a666e24ffbbc9175845284194e02b3d3
Felix Pham <felix.p@academyxi.com>
2022-09-13 10:05:39 +0700

    create app

---
+++ pages/404.js
@@ -0,0 +1,5 @@
+import utilStyles from '../styles/utils.module.css';
+
+export default function Custom404() {
+    return <div className='text-center'><h1 className={utilStyles.headingXl}>404 - Page Not Found</h1></div>;
+  }
